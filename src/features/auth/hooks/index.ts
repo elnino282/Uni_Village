8aac1b103d400e97421cc975427a7df14e36ee1f
Quoosc <quocitphanmem@gmail.com>
2026-01-08 23:10:27 +0700

    Export new auth hooks

--- src/features/auth/hooks/index.ts
+++ src/features/auth/hooks/index.ts
@@ -1,4 +1,7 @@
 export { useAuth } from './useAuth';
 export { useLogin } from './useLogin';
 export { useRegister } from './useRegister';
+export { useVerifyOtpRegister } from './useVerifyOtpRegister';
+export { useForgotPassword } from './useForgotPassword';
+export { useVerifyOtpForgotPassword } from './useVerifyOtpForgotPassword';
 
