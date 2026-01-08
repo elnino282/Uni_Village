1dd0c62676ff67bc5d4691657bb14208bd7ad3c6
Quoosc <quocitphanmem@gmail.com>
2026-01-08 23:14:27 +0700

    Refine module alias mapping

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,29 @@
+// babel.config.js
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: ['babel-preset-expo'],
-        plugins: [
-            [
-                'module-resolver',
-                {
-                    root: ['./'],
-                    alias: {
-                        '@': './',
-                        '@/src': './src',
-                        '@/features': './src/features',
-                        '@/shared': './src/shared',
-                        '@/lib': './src/lib',
-                        '@/config': './src/config',
-                        '@/store': './src/store',
-                        '@/providers': './src/providers',
-                    },
-                },
-            ],
-        ],
-    };
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@/features": "./src/features",
+            "@/shared": "./src/shared",
+            "@/lib": "./src/lib",
+            "@/config": "./src/config",
+            "@/store": "./src/store",
+            "@/providers": "./src/providers",
+            "@/src": "./src",
+            "@/components": "./components",
+            "@/hooks": "./hooks",
+            "@/constants": "./constants",
+            "@/assets": "./assets",
+            "@": "./",
+          },
+        },
+      ],
+    ],
+  };
 };
