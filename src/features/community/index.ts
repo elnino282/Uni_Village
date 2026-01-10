306225b0f0569a325368136d96fd354a6d18e254
elnino282 <23521333@gm.uit.edu.vn>
2026-01-11 03:20:07 +0700

    Update index.ts

--- src/features/community/index.ts
+++ src/features/community/index.ts
@@ -1,7 +1,7 @@
 // Community feature barrel export
 
 // Components - selectively export to avoid name conflicts
-export { CommunityFAB, CommunityHeader, PostActions as CommunityPostActions, PostCard as CommunityPostCard, PostHeader as CommunityPostHeader, PostLocations as CommunityPostLocations, PostMedia as CommunityPostMedia, CommunityScreen, CommunitySearchBar, CommunitySegmentedTabs, MessagesTab, PostOverflowMenu } from './components';
+export { AddFriendScreen, CommunityFAB, CommunityHeader, PostActions as CommunityPostActions, PostCard as CommunityPostCard, PostHeader as CommunityPostHeader, PostLocations as CommunityPostLocations, PostMedia as CommunityPostMedia, CommunityScreen, CommunitySearchBar, CommunitySegmentedTabs, MessagesTab, PlusActionMenu, PostOverflowMenu } from './components';
 
 // Hooks
 export * from './hooks';
