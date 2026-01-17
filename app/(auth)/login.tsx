52a860a98d05cea1bc0fa55f987bc98fb980028b
elnino282 <23521333@gm.uit.edu.vn>
2026-01-18 05:31:14 +0700

    Add login and register route wrappers
    Introduced thin wrapper components for LoginScreen and RegisterScreen under the (auth) directory to handle login and registration routes.

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,8 @@
+/**
+ * Login Route
+ * Thin wrapper for LoginScreen
+ */
+
+import { LoginScreen } from '@/features/auth';
+
+export default LoginScreen;
---
+++ app/(auth)/register.tsx
@@ -0,0 +1,8 @@
+/**
+ * Register Route
+ * Thin wrapper for RegisterScreen
+ */
+
+import { RegisterScreen } from '@/features/auth';
+
+export default RegisterScreen;
