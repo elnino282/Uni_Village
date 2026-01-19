8e3862b2f70e0ad4ff64628dd821a46f953095ac
elnino282 <23521333@gm.uit.edu.vn>
2026-01-20 03:33:41 +0700

    Export useProfileShareSheet from hooks index
    Added useProfileShareSheet to the exports in the profile hooks index file to make it available for import elsewhere in the application.

--- src/features/profile/hooks/index.ts
+++ src/features/profile/hooks/index.ts
@@ -3,6 +3,7 @@ export { useImagePicker } from './useImagePicker';
 export type { ImagePickerResult, UseImagePickerOptions } from './useImagePicker';
 export { profileKeys, useMyProfile, useProfile } from './useProfile';
 export { profilePostsKeys, useProfilePosts } from './useProfilePosts';
+export { useProfileShareSheet } from './useProfileShareSheet';
 export { publicProfileKeys, usePublicProfile } from './usePublicProfile';
 export { useUpdateProfile } from './useUpdateProfile';
 export { useUploadAvatar, useUploadCover } from './useUploadImage';
