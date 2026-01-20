0dd5b0f0305741cc0b14e39b1c5b5f5fa985c013
elnino282 <23521333@gm.uit.edu.vn>
2026-01-20 12:29:43 +0700

    Add routes for message and friend requests
    Introduces new route components for message requests and friend requests. MessageRequestsRoute renders the MessageRequestsScreen, while FriendRequestsRoute sets up a screen with a title and displays the FriendRequestsList component.

---
+++ app/chat/message-requests.tsx
@@ -0,0 +1,9 @@
+/**
+ * Message Requests Route
+ */
+import { MessageRequestsScreen } from '@/features/chat/screens/MessageRequestsScreen';
+import React from 'react';
+
+export default function MessageRequestsRoute() {
+    return <MessageRequestsScreen />;
+}
