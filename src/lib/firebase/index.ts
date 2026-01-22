dbefd840c680ead06503ce00d8d68402f33a71d8
elnino282 <23521333@gm.uit.edu.vn>
2026-01-22 17:14:02 +0700

    Resolve merge conflict in Firebase exports
    Merged conflicting changes in src/lib/firebase/index.ts to ensure all necessary exports (auth, firebaseApp, firestore, storage, getCurrentSession, getCurrentUser) are included from './client'.

--- src/lib/firebase/index.ts
+++ src/lib/firebase/index.ts
@@ -3,7 +3,11 @@
  */
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 export { auth, database, firebaseApp, firestore, getCurrentSession, getCurrentUser } from './client';
+=======
+export { auth, firebaseApp, firestore, storage, getCurrentSession, getCurrentUser } from './client';
+>>>>>>> Stashed changes
 =======
 export { auth, firebaseApp, firestore, storage, getCurrentSession, getCurrentUser } from './client';
 >>>>>>> Stashed changes
